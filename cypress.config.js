f88c875aef4f4b44f5884d90def0aea5d0f1ef12
Sudha <sudaneupane.11@gmail.com>
2024-05-31 18:06:27 +0545

    basics

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   e2e: {
+    watchForFileChanges:false,
+    defaultCommandTimeout:3000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -2,4 +2,7 @@
 
 it('Google Search',()=>{
     cy.visit('https://google.com')
+    cy.get('#APjFqb',{timeout:5000}).type('Automation step by Step{Enter}')
+    // cy.get('.FPdoLc > center > .gNO89b').click()
+    cy.get('.Lu57id').click()
 })
